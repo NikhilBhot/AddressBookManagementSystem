80dcf06568d955a4794a125dc89a3a9831e2335e
unknown <bhotnikhil25@gmail.com>
2023-03-06 10:05:55 +0530

    [Nikhil]-Ability To Create Contact

--- AddressBookManagementSystem/Program.cs
+++ AddressBookManagementSystem/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace AddressBookManagementSystem
+﻿using AddressBookManagementSystem;
+
+namespace AddressBookManagementSystem
 {
-    public class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book Management Systaem Programming");
+            Console.WriteLine("Welcome to address Book System");
+           /*UC-01 Ability to create a Contacts in Address Book with first and last names, address,city, state, zip, 
+            phone number and email...
+           */
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.CreatContact();
         }
     }
 }
